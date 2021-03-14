08000243e85e4223e5061e1b9a4295ab974e0dae
caioharuo <caio.haruo@hotmail.com>
2021-03-14 14:42:00 -0300

    retirando limite de msgs

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ function ChatRoom() {
   const dummy = useRef();
 
   const messagesRef = firestore.collection("messages");
-  const query = messagesRef.orderBy("createdAt").limit(25);
+  const query = messagesRef.orderBy("createdAt");
 
   const [messages] = useCollectionData(query, { idField: "id" });
 
